8b29a851e922ac57a517987858ac1796fa15a4f1
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2015-07-21 19:44:33 +0300

    v1.8

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 // Version number.
-VERSION = '1.8-beta';
+VERSION = '1.8';
 // Copyright year.
 YEAR = 2015;
 // If set, be silent and be more debugging friendly.
@@ -13,7 +13,7 @@ CHANGELOG = [{
             'New 50:10, 55:05 and 60:00 schedules for fitness.',
             'Use less CPU.',
             'Code clean up in various places.',
-            'Improve CSS to support mobile devices better.',
+            'New nice CSS to also support mobile devices better.',
             'Loading screen with nice logo.',
         ]
     },{
